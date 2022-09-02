2bd5b3181faf3455fbf45473fe9b9d15db8c1723
Ali Gholami Rudi <ali@rudi.ir>
2022-09-02 15:02:17 +0430

    mcmd: clean up global variables

--- mcmd.py
+++ mcmd.py
@@ -13,6 +13,7 @@
 # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+import math
 import sys
 import pulp
 
@@ -21,11 +22,9 @@ sigma = []		# sigma[i]: sorted points based on distance to disk[i]
 rigma = []		# reverse of sigma
 rads = []		# disk radii
 cens = []		# disk centres
-n = 0			# number of disks
 dist_haver = False	# use haversine formula for distance
 
 def haversine(a, b):
-	import math
 	R = 6371230;
 	a1 = math.radians(a[0])
 	o1 = math.radians(a[1])
@@ -41,53 +40,48 @@ def dist(a, b):
 	return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
 
 def init_arrays():
-	global n
-	global sigma
-	global rigma
-	global rads
-	global cens
 	n = len(disks)
-	cens = [d[0] for d in disks]
-	rads = [d[1] for d in disks]
-	sigma = [None] * n
-	for i in range(n):
+	cens.extend([d[0] for d in disks])
+	rads.extend([d[1] for d in disks])
+	sigma.extend([None] * len(disks))
+	for i in range(len(disks)):
 		sigma[i] = list(range(len(disks)))
 		sigma[i].sort(key=lambda p: dist(cens[i], cens[p]))
-	for i in range(n):
+	for i in range(len(disks)):
 		rad = 0
 		for j in range(len(sigma[i])):
 			if j > 0 and rad < dist(cens[i], cens[sigma[i][j]]):
 				del sigma[i][j:]
 				break
 			rad += rads[sigma[i][j]]
-	rigma = [list() for i in range(n)]
-	for i in range(n):
+	rigma.extend([list() for i in range(len(disks))])
+	for i in range(len(disks)):
 		for j in sigma[i]:
 			rigma[j].append(i)
 
 def linprog():
 	prob = pulp.LpProblem("MergeableDisks", pulp.LpMinimize)
-	vars_list = [(j, i) for i in range(n) for j in sigma[i]]
+	vars_list = [(j, i) for i in range(len(disks)) for j in sigma[i]]
 	# x[i, j] indicates that disk i is merged with disk j
 	x = pulp.LpVariable.dicts("x", vars_list, 0, 1, cat="Integer")
 	# Each disk is merged at most once
-	for i in range(n):
+	for i in range(len(disks)):
 		prob += pulp.lpSum(x[i, j] for j in rigma[i]) == 1
 	# A can be merged with B if all closer disks to B are also merged with it
-	for i in range(n):
+	for i in range(len(disks)):
 		for j in range(len(sigma[i]) - 1):
 			prob += x[sigma[i][j], i] >= x[sigma[i][j + 1], i]
 	# Disks must be centre-disjoint
-	for i in range(n):
-		for j in range(n):
+	for i in range(len(disks)):
+		for j in range(len(disks)):
 			if i != j:
 				d = dist(cens[i], cens[j]) + 1000000 * (1 - x[j, j])
 				prob += pulp.lpSum(x[k, i] * rads[k] for k in sigma[i]) <= d
 	# Maximize selected disks
-	prob += pulp.lpSum(-x[i, i] for i in range(n))
+	prob += pulp.lpSum(-x[i, i] for i in range(len(disks)))
 	prob.solve()
-	phi = [-1] * n
-	for i in range(n):
+	phi = [-1] * len(disks)
+	for i in range(len(disks)):
 		for j in sigma[i]:
 			if x[j, i].value():
 				phi[j] = i
@@ -108,13 +102,13 @@ if __name__ == "__main__":
 	init_arrays()
 	phi = linprog()
 	print("Merges:")
-	for i in range(n):
+	for i in range(len(disks)):
 		if phi[i] != i:
 			print(f"  {i} -> {phi[i]}")
-	outs = [0 for i in range(n)]
-	for i in range(n):
+	outs = [0 for i in range(len(disks))]
+	for i in range(len(disks)):
 		outs[phi[i]] += rads[i]
 	print("Resulting disks:")
-	for i in range(n):
+	for i in range(len(disks)):
 		if outs[i]:
 			print(f"  {cens[i][0]} {cens[i][1]}  {outs[i]}")
